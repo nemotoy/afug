adbc39399fdfe604912699fe0e8346ce86b1a3ea
yuma-nemoto <yuma-nemoto@dac.co.jp>
2021-03-09 23:20:30 +0900

    chore: print each repo

--- main.go
+++ main.go
@@ -16,5 +16,11 @@ func main() {
 		fmt.Println(err)
 		os.Exit(1)
 	}
-	fmt.Printf("users: %+v", users)
+	var id int
+	for _, user := range users {
+		for _, repo := range user.StarredRepositories.Nodes {
+			id++
+			fmt.Printf("id: %d, name: %s, url: %s, lang: %s\n", id, user.Name, repo.URL, repo.PrimaryLanguage.Name)
+		}
+	}
 }
