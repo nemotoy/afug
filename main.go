4d451571237776eed8bdac8133f08dc30dd95db1
yuma-nemoto <yuma-nemoto@dac.co.jp>
2021-03-10 08:19:41 +0900

    tmp: call tui app

--- main.go
+++ main.go
@@ -4,8 +4,10 @@ import (
 	"context"
 	"fmt"
 	"os"
+	"time"
 
 	gh "github.com/nemotoy/afug/github"
+	"github.com/nemotoy/afug/tui"
 )
 
 func main() {
@@ -16,11 +18,21 @@ func main() {
 		fmt.Println(err)
 		os.Exit(1)
 	}
+
 	var id int
 	for _, user := range users {
 		for _, repo := range user.StarredRepositories.Nodes {
 			id++
 			fmt.Printf("id: %d, name: %s, url: %s, lang: %s\n", id, user.Name, repo.URL, repo.PrimaryLanguage.Name)
 		}
+	}
+
+	time.Sleep(3 * time.Second)
+
+	app := tui.NewAppWithWidget()
+	app.SetUsers(users)
+	if err := app.Run(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
 	}
 }
--- tui/tui.go
+++ tui/tui.go
@@ -1,31 +1,54 @@
 package tui
 
 import (
+	gh "github.com/nemotoy/afug/github"
 	"github.com/rivo/tview"
 )
 
 type TUI struct {
-	app   *tview.Application
-	table *tview.Table
+	app    *tview.Application
+	table  *tview.Table
+	layout *tview.Grid
 }
 
 func NewAppWithWidget() *TUI {
 	app := tview.NewApplication()
 
 	table := tview.NewTable().
+		SetBorders(false)
+
+	layout := tview.NewGrid().
+		SetRows(3, 0, 3).
+		SetColumns(30, 0, 30).
 		SetBorders(false).
-		SetCell(0, 0,
-			tview.NewTableCell("ID").
-				SetAlign(tview.AlignLeft)).
-		SetCell(0, 1,
-			tview.NewTableCell("Language").
-				SetAlign(tview.AlignLeft)).
-		SetCell(0, 2,
-			tview.NewTableCell("URL").
-				SetAlign(tview.AlignLeft))
+		AddItem(table, 1, 0, 1, 3, 0, 100, false)
 
 	return &TUI{
-		app:   app,
-		table: table,
+		app:    app,
+		table:  table,
+		layout: layout,
+	}
+}
+
+func (t *TUI) Run() error {
+	return t.app.SetRoot(t.layout, true).SetFocus(t.table).Run()
+}
+
+// TODO: handle githubv4.URI
+func (t *TUI) SetUsers(users []gh.User) {
+
+	var id int = 1
+	for _, user := range users {
+		for _, repo := range user.StarredRepositories.Nodes {
+			t.table.SetCell(id, 0,
+				tview.NewTableCell(string(user.Name)).
+					SetAlign(tview.AlignLeft))
+			t.table.SetCell(id, 1,
+				tview.NewTableCell(string(repo.PrimaryLanguage.Name)).
+					SetAlign(tview.AlignLeft))
+			t.table.SetCell(id, 2,
+				tview.NewTableCell("-").
+					SetAlign(tview.AlignLeft))
+		}
 	}
 }
