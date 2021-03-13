5da4a4b425003c42d9cfcfdd538625e01ae938c7
yuma-nemoto <yuma-nemoto@dac.co.jp>
2021-03-13 09:09:27 +0900

    fix: log message

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -14,15 +14,16 @@ func Execute() {
 	cli := gh.NewClient(context.Background(), os.Getenv("GITHUB_TOKEN"))
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
+
 	users, err := cli.GetUsersStarredRepos(ctx)
 	if err != nil {
-		fmt.Println(err)
+		fmt.Printf("fetch users failed: %v\n", err)
 		os.Exit(1)
 	}
 
 	app := tui.NewAppWithWidget().SetTableFrame().SetUsers(users)
 	if err := app.Run(); err != nil {
-		fmt.Println(err)
+		fmt.Printf("app failed: %v\n", err)
 		os.Exit(1)
 	}
 }
